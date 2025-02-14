605c689fe993888853a1c87bdd85ff448db10777
Dannick <dannick.kluijtmans@gmail.com>
2025-02-14 11:06:38 +0100

    Added some comments

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import aiohttp
 # Constants
 CONFIG_FILE = 'config.json' #name of the config file. The file will be created on the first run of the bot.
 TMUX_SESSION = 'arma_reforger' #give the name of the tmux session that is used for your game server
-LOG_FILE = 'bot.log' #Name of the log file
+LOG_FILE = 'bot.log' #Name of the log file. Created in the same directory
 
 # Set up logging
 logger = logging.getLogger('TMuxBot')
